4398c3becdf31922b38f189a771a62ec3a80cf49
larajuneb <larajuneb@gmail.com>
2020-03-18 17:27:26 +0200

    added libs & samples

--- src/traversal/Traversal.java
+++ src/traversal/Traversal.java
@@ -1,5 +1,31 @@
 package traversal;
 
+import princeton.In;
+
 public class Traversal {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length == 2) {
+			// Text mode
+			In in = new In(args[0]);
+			System.out.println(in.readLine());
+			int R = in.readInt();
+			int C = in.readInt();
+			for (int r = 0; r < R; r++) {
+				char dummy = in.readChar();
+				for (int c = 0; c < C; c++) {
+					char obs = in.readChar();
+					System.out.println("r == " + r + " c == " + c + " obs == "
+							+ obs);
+				}
+			}
+		} else {
+			System.out.println("Usage: java Keys [boardfile] [commandfile]");
+		}
+	}
 
 }
